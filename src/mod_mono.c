e0d7863cbb4b0166182c6b03dcd478a36dd8dbd5
Marek Habersack <grendel@twistedcode.net>
2007-03-28 22:00:46 +0000

    2007-03-28  Marek Habersack  <grendello@gmail.com>
    	* src/mod_mono.c (do_command): abort when either xsp sends us size
    	of -1 in the GET_CLIENT_BLOCK command or when ap_get_client_block
    	returns -1 as the result of client breaking the connection. This
    	action prevents an exception later on in xsp.
    
    
    svn path=/trunk/mod_mono/; revision=75117

--- src/mod_mono.c
+++ src/mod_mono.c
@@ -688,8 +688,16 @@ do_command (int command, apr_socket_t *sock, request_rec *r, int *result)
 			break;
 
 		i = INT_FROM_LE (i);
+		if (i < 0) {
+			DEBUG_PRINT (2, "xsp sent us size == %d: not processing", i);
+			abort();
+		}
 		str = get_client_block_buffer (r, (uint32_t) i, &actual_size);
 		i = ap_get_client_block (r, str, actual_size);
+		if (i < 0) {
+			DEBUG_PRINT (2, "our block has size %d: not sending to xsp", i);
+			abort();
+		}
 		i = LE_FROM_INT (i);
 		status = write_data (sock, &i, sizeof (int32_t));
 		i = INT_FROM_LE (i);
